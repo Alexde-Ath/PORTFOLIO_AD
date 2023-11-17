c8f4a47b7a32e6f7d3acfc5a57d847401d1f7874
Alexandra d <adeath@u.rochester.edu>
2023-11-17 15:58:43 -0500

    Update: README Update: files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter } from 'react-router-dom'; // get domain
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className = "relative z-0 bq-primary">
+        <div className = "bg-hero-pattern bg-cover bq-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About/>
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className = "relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+import{ Link } from 'react-router-dom';
+import{ styles } from '../styles';
+import{ navLinks } from '../constants';
+import{ logo, menu, close } from '../assets';
+/**
+ * 
+ */
+const Navbar = () => {
+  return (
+    /* Class parameters from Tailwind */
+    <nav 
+    className ={'${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg=primary'}
+    >
+      <div 
+      className = "w-full flex justify-between items-center max-w-7xl mx-auto"
+      >
+        <Link
+        to="/"
+        className= "flex items-center gap-2"
+        onClick={()=>{
+          setActive("");
+          window.scrollTo(0,0) // Set to top page
+        }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
