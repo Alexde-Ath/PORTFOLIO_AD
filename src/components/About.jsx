b42bbd5d36d2edb0e0042e69ed65f4053ca5f7b9
Alexandra d <adeath@u.rochester.edu>
2023-12-24 14:14:19 -0500

    FIX: wording

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='font-serif mt-4 text-tertiary text-[17px] max-w-3xl leading-[30px]'
       >
-        I am a junior studying computer science at University of Rochester and I enjoy playing the flute, soccer, and taste-testing food. I am looking to apply experiences from my academic classes in the real world, some classes I took: Data Structures & Algorithms, Computation & Formal Systems, and Artifical Intelligence. I have varying profiency in these languages: C, Java, Python, and I also seperately learnt how to use R, MATLAB, and SQL. As displayed by this website I have also learned more about HTML, CSS. Reactjs, which I am interested in pursuing further. 
+        I am a junior studying computer science at University of Rochester and I enjoy playing the flute, soccer, and taste-testing food. I am looking to apply experiences from my academic classes in the real world, some classes I took: Data Structures & Algorithms, Computation & Formal Systems, and Artifical Intelligence. I have varying profiency in these languages: C, Java, Python, and I also seperately learnt how to use R, MATLAB, and SQL. As displayed by this website I have also have some experience HTML, CSS. Reactjs, which I am interested in pursuing further. 
         <span 
         variants={fadeIn("", "", 0.1, 1)}
         className='font-serif mt-4 text-tertiary text-[17px] max-w-3xl leading-[30px] flex'> I would love to work in person and willing to travel. Please feel free to contact me for job oppurtunities or just a chat about tasty food! </span>
