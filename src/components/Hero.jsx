0a7c7953ac840e6a8d0b55872cacbedfb0a1ea0f
Alexde-Ath <134790249+Alexde-Ath@users.noreply.github.com>
2024-09-04 13:27:18 -0400

    redoing hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { styles } from "../styles";
 
+/**
+ * HERO
+ */
 const Hero = () => {
 
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div className={`absolute inset-0 top-[120px]  max-w-8xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-        <div className = {''}>
-          HELLO WORLD!
-          <button></button>
-        </div>
-        <div>
-          I do CS stuff...
-        </div>
+      <div className = {'ml-100 mt-64'}>
       </div>
     </section>
   );
